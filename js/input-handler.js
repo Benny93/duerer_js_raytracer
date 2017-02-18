dc69cfb57ffdf4807dc4f4a71608749f1f252df0
Benny93 <vollmerbenjamin93@gmail.com>
2017-02-18 21:40:06 +0100

    Saved preset for toon shader

--- js/input-handler.js
+++ js/input-handler.js
@@ -22,8 +22,8 @@ function inputButtonPressedSetPreset() {
         case "3": //toon preset
             document.getElementsByName('shaderType')[0].value = 2;
             document.getElementsByName('castShadows')[0].value = 1;
-            document.getElementsByName('imageWidth')[0].value = 40;
-            document.getElementsByName('imageHeight')[0].value = 30;
+            document.getElementsByName('imageWidth')[0].value = 400;
+            document.getElementsByName('imageHeight')[0].value = 300;
             document.getElementsByName('renderDepth')[0].value = 1;
             break;
         default:
